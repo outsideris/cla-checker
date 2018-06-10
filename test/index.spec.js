66708b19ed8a04eb00db31ec04a947d3b024ddda
Outsider <outsideris@gmail.com>
2018-06-10 20:42:06 +0900

    nest a new test suite
    Signed-off-by: Outsider <outsideris@gmail.com>

--- test/index.spec.js
+++ test/index.spec.js
@@ -7,50 +7,51 @@ const payload = require('./fixtures/pr-payload.json')
 describe('cla-check', () => {
   let robot
   let github
-  const notFound = new Error('Not Found')
-  notFound.code = 404
-  const configFile = `
-  url: https://gist.githubusercontent.com/outsideris/3219615d9fc251aaf29177d3eb7c1981/raw/84650bac1cec3941952743e2c35d3e276c834eec/cla-test.txt
-  `
 
-  beforeEach(() => {
-    robot = createRobot()
-    plugin(robot)
+  describe('with config file', () => {
+    const configFile = `
+    url: https://gist.githubusercontent.com/outsideris/3219615d9fc251aaf29177d3eb7c1981/raw/84650bac1cec3941952743e2c35d3e276c834eec/cla-test.txt
+    `
 
-    github = {
-      repos: {
-        getContent: jest.fn().mockReturnValue({
-          data: {
-            content: Buffer.from(configFile).toString('base64')
-          }
-        }),
-        compareCommits: jest.fn().mockReturnValue(Promise.resolve({
-          data: {
-            commits: [
-              {
-                sha: 'be7ef9a',
-                commit: {
-                  author: { name: 'John', email: 'john@example.com' },
-                  committer: { name: 'John', email: 'john@example.com' },
-                  message: 'this is commit message'
+    beforeEach(() => {
+      robot = createRobot()
+      plugin(robot)
+
+      github = {
+        repos: {
+          getContent: jest.fn().mockReturnValue({
+            data: {
+              content: Buffer.from(configFile).toString('base64')
+            }
+          }),
+          compareCommits: jest.fn().mockReturnValue(Promise.resolve({
+            data: {
+              commits: [
+                {
+                  sha: 'be7ef9a',
+                  commit: {
+                    author: { name: 'John', email: 'john@example.com' },
+                    committer: { name: 'John', email: 'john@example.com' },
+                    message: 'this is commit message'
+                  }
                 }
-              }
-            ]
-          }
-        })),
-        createStatus: jest.fn().mockReturnValue({})
-      },
-      issues: {
-        createComment: jest.fn().mockReturnValue(Promise.resolve({}))
+              ]
+            }
+          })),
+          createStatus: jest.fn().mockReturnValue({})
+        },
+        issues: {
+          createComment: jest.fn().mockReturnValue(Promise.resolve({}))
+        }
       }
-    }
-    robot.auth = () => Promise.resolve(github)
-  })
+      robot.auth = () => Promise.resolve(github)
+    })
 
-  it('check committers and leave a comment', async () => {
-    await robot.receive(payload)
+    it('check committers and leave a comment', async () => {
+      await robot.receive(payload)
 
-    expect(github.issues.createComment).toHaveBeenCalled()
-    expect(github.repos.createStatus).toHaveBeenCalled()
+      expect(github.issues.createComment).toHaveBeenCalled()
+      expect(github.repos.createStatus).toHaveBeenCalled()
+    })
   })
 })
